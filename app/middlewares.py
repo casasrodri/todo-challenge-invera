b15f532da5fce54cd9a9db720be2258ed9a248a9
Rodri Casas <cr.rodrigocasas@gmail.com>
2024-10-27 17:09:17 -0300

    improve obfuscating func and re-order logger logic

--- app/middlewares.py
+++ app/middlewares.py
@@ -10,7 +10,7 @@ def obfuscate_sensitive_data(data: bytes | dict) -> str:
     """Obfuscate sensitive data from the request/response.
 
     Args:
-        data (bytes | dict): Data to obfuscate (body of request/response).
+        data (str | dict): Data to obfuscate (body of request/response).
 
     Raises:
         Exception: Data must be a bytes or dict.
@@ -18,19 +18,16 @@ def obfuscate_sensitive_data(data: bytes | dict) -> str:
     Returns:
         str: Obfuscated data with **** or ABC123.
     """
-    if isinstance(data, bytes):
-        data = json.loads(data.decode("utf-8"))
+    if isinstance(data, str):
+        data = json.loads(data)
     elif isinstance(data, dict):
         pass
     else:
         raise Exception(f"Data must be a bytes or dict, not '{type(data)}'.")
 
-    if "username" in data and "password" in data:
-        data["password"] = "********"
-
-    if "refresh" in data and "access" in data:
-        data["refresh"] = "123.ABC"
-        data["access"] = "ABC.123"
+    for field in ("password", "refresh", "access"):
+        if field in data:
+            data[field] = "****"
 
     return json.dumps(data)
 
@@ -39,24 +36,36 @@ class LogRequestResponseMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
+    def __log_request(self, request, path):
+        logger.info(f"Request:\t\t{request.method}\t{path}")
+        if request.body:
+            body = request.body.decode("utf-8")
+            if "/api/token/" in path:
+                body = obfuscate_sensitive_data(body)
+
+            logger.debug(f"Request body:\t{body}")
+
+    def __log_response(self, response, path, duration):
+        logger.info(f"Response:\t\t{response.status_code}\t{path} | {duration:.2f}s")
+        if hasattr(response, "data"):
+            body = response.data
+            if "/api/token/" in path:
+                body = obfuscate_sensitive_data(body)
+                print(body)
+            logger.debug(f"Response body:\t{body}")
+
     def __call__(self, request):
         start_time = time.time()
         path = request.get_full_path()
 
         # Log of the request
-        logger.info(f"Request:\t\t{request.method}\t{path}")
-        if request.body:
-            body = obfuscate_sensitive_data(request.body)
-            logger.debug(f"Request body:\t{body}")
+        self.__log_request(request, path)
 
         # Get the response
         response = self.get_response(request)
         duration = time.time() - start_time
 
         # Log of the response
-        logger.info(f"Response:\t\t{response.status_code}\t{path} | {duration:.2f}s")
-        if hasattr(response, "data"):
-            body = obfuscate_sensitive_data(response.data)
-            logger.debug(f"Response body:\t{body}")
+        self.__log_response(response, path, duration)
 
         return response
